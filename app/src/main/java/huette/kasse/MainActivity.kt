e56ee30132a4dcbea5cff0059dc465bd1115c54d
hitmarcer <xmarcbx99@gmail.com>
2021-07-26 17:53:46 +0200

    Commit Mac

--- app/src/main/java/huette/kasse/MainActivity.kt
+++ app/src/main/java/huette/kasse/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     var alBtnDrinks: ArrayList<Button> = ArrayList<Button>()
     var alUsers: ArrayList<User> = ArrayList<User>()
     var alDrinks: ArrayList<Drink> = ArrayList<Drink>()
+    //Test
 
     val pw = ""
 
